21c86ecf6837383db5d2af27076f84ef66883900
Harry Digos <harrydigos@gmail.com>
2025-12-08 21:09:39 +0200

    day 4 part 2

--- day04/main.go
+++ day04/main.go
@@ -19,22 +19,23 @@ func main() {
 		grid = append(grid, []rune(line))
 	}
 
-	fmt.Println("Part 1:", solvePart1(grid))
+	fmt.Println("Part 1:", solvePart1(grid)) // 1395
+	fmt.Println("Part 2:", solvePart2(grid)) // 8451
+}
+
+var directions = [][2]int{
+	{-1, 0},  // up
+	{1, 0},   // down
+	{0, -1},  // left
+	{0, 1},   // right
+	{-1, -1}, // up-left
+	{-1, 1},  // up-right
+	{1, -1},  // down-left
+	{1, 1},   // down-right
 }
 
 func solvePart1(grid [][]rune) int {
 	total := 0
-
-	directions := [][2]int{
-		{-1, 0},  // up
-		{1, 0},   // down
-		{0, -1},  // left
-		{0, 1},   // right
-		{-1, -1}, // up-left
-		{-1, 1},  // up-right
-		{1, -1},  // down-left
-		{1, 1},   // down-right
-	}
 
 	for i := range grid {
 		for j := range grid[i] {
@@ -57,5 +58,46 @@ func solvePart1(grid [][]rune) int {
 			}
 		}
 	}
+	return total
+}
+
+type Position struct {
+	row int
+	col int
+}
+
+func solvePart2(grid [][]rune) int {
+	total := 0
+	removed := make(map[Position]bool)
+	lastRoundFound := 1
+
+	for lastRoundFound > 0 {
+		lastRoundFound = 0
+
+		for i := range grid {
+			for j := range grid[i] {
+				if (grid[i][j] != '@' || removed[Position{i, j}]) {
+					continue
+				}
+
+				foundPapers := 0
+				for _, dir := range directions {
+					ni, nj := i+dir[0], j+dir[1]
+					if ni >= 0 && ni < len(grid) && nj >= 0 && nj < len(grid[ni]) {
+						if grid[ni][nj] == '@' && !removed[Position{ni, nj}] {
+							foundPapers++
+						}
+					}
+				}
+
+				if foundPapers < 4 {
+					total++
+					removed[Position{i, j}] = true
+					lastRoundFound++
+				}
+			}
+		}
+	}
+
 	return total
 }

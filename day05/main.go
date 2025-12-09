da54bc10486d2a0855b693fa0a4c16a3dd795b37
Harry Digos <harrydigos@gmail.com>
2025-12-09 19:44:26 +0200

    day 5 part 1

---
+++ day05/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("day05/input.txt")
+	defer file.Close()
+
+	var ranges [][2]int
+	var items []int
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			break
+		}
+
+		parts := strings.Split(line, "-")
+		if len(parts) != 2 {
+			continue
+		}
+
+		start, _ := strconv.Atoi(parts[0])
+		end, _ := strconv.Atoi(parts[1])
+
+		ranges = append(ranges, [2]int{start, end})
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		item, _ := strconv.Atoi(line)
+		items = append(items, item)
+	}
+
+	fmt.Println("Part 1:", solvePart1(ranges, items)) // 735
+}
+
+func solvePart1(ranges [][2]int, items []int) int {
+	total := 0
+
+	for _, item := range items {
+		foundAlready := false
+		for _, r := range ranges {
+			if item >= r[0] && item <= r[1] && !foundAlready {
+				total++
+				foundAlready = true
+			}
+		}
+	}
+
+	return total
+}
